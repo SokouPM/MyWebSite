b46b0ee8ac9ae6d441bcbae56a47020bba26c76f
Pierre Marquet <marquet_pierre@yahoo.fr>
2020-10-15 23:12:20 +0200

    update home page

---
+++ library/js/animation.js
@@ -0,0 +1,26 @@
+var hat = document.getElementById('hat');
+var pm = document.getElementById('pm');
+var textWrapper = document.getElementById('welcomeTitle');
+var textWrapper2 = document.getElementById('clickToGo');
+
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+textWrapper2.innerHTML = textWrapper2.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '#welcomeTitle .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 100,
+        delay: (el, i) => 50 * (i+1)
+    });
+
+anime.timeline({ loop: false })
+    .add({
+        targets: '#clickToGo .letter',
+        opacity: [0,1],
+        easing: "easeInOutQuad",
+        duration: 50,
+        delay: (el, i) => 25 * (i+1)
+    });
+
