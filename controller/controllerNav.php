cbef18f2f3350e16aa512f609bfe44c23237d26c
Pierre Marquet <marquet_pierre@yahoo.fr>
2020-10-18 16:30:44 +0200

    update

--- controller/controllerNav.php
+++ controller/controllerNav.php
@@ -2,23 +2,23 @@
 
 switch (key($_GET)) {   // If in the url there is :
     case 'accueil':
-        require_once('view/home.php');       // Go to page "view/whoami.php"
+        require_once('view/home.phtml');       // Go to page "view/whoami.php"
         break;
 
     case 'presentation':
-        require_once('view/whoami.php');       // Go to page "view/whoami.php"
+        require_once('view/whoami.phtml');       // Go to page "view/whoami.php"
         break;
 
     case 'parcours':
-        require_once('view/mycareer.php');     // Go to page "view/mycareer.php"
+        require_once('view/mycareer.phtml');     // Go to page "view/mycareer.php"
         break;
 
     case 'infographies':
-        require_once('view/infographics.php');     // Go to page "view/mycareer.php"
+        require_once('view/infographics.phtml');     // Go to page "view/mycareer.php"
         break;
 
     case 'web':
-        require_once('view/web.php');     // Go to page "view/web.php"
+        require_once('view/web.phtml');     // Go to page "view/web.php"
         break;
 
     default:
