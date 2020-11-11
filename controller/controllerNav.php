66f0905276a1bcc2895b743cbac998a3e698f957
Pierre Marquet <marquet_pierre@yahoo.fr>
2020-11-11 01:03:26 +0100

    update

--- controller/controllerNav.php
+++ controller/controllerNav.php
@@ -64,16 +64,16 @@ class ControllerNav
         }
     }
 
-    public static function activateSnow(): void
+    public static function toggleSnow(): void
     {
         $today = new DateTime();
         $endDate = new DateTime(date('y') . '-03-01');
         $startDate = new DateTime(date('y') . '-11-01');
 
-        if ($startDate <= $today && $today <= $endDate) {
-            $_SESSION["snow"] = true;
-        } else {
+        if ($today <= $startDate && $today >= $endDate) {
             $_SESSION["snow"] = false;
+        } else {
+            $_SESSION["snow"] = true;
         }
     }
 }
--- index.php
+++ index.php
@@ -7,5 +7,5 @@ session_start();
 header('content-type: text/html; charset=utf-8');   // to get accents
 
 require_once('controller/controllerNav.php');       // to run function from "controller/controllerNav.php"
-ControllerNav::activateSnow();
+ControllerNav::toggleSnow();
 ControllerNav::runNavigation();
